ff233eaacab0aeca14f88aa601ff517803486645
kosin <statismike@gmail.com>
2022-05-03 22:05:04 +0200

    added codecov and fixed bugs

---
+++ tests/testthat/test-FrequencyTable.R
@@ -0,0 +1,67 @@
+ft <- NULL
+ft_sim <- NULL
+
+test_that("FrequencyTable can be constructed from raw values", {
+  
+  expect_warning(
+    ft <<- FrequencyTable(HEXACO_60$HEX_H)  
+  )
+  
+  expect_s3_class(
+    ft, "FrequencyTable"
+  )
+})
+
+test_that("FrequencyTable can be simulated", {
+  
+  ft_sim <<- SimFrequencyTable(
+    min = 10,
+    max = 50,
+    M = 30,
+    SD = 7
+  )
+  
+  expect_s3_class(
+    ft_sim, c("FrequencyTable", "Simulated")
+  )
+  
+})
+
+test_that("FrequencyTable prints", {
+  
+  expect_output(
+    print(ft),
+    regex = paste0(
+      "^<FrequencyTable>.*", ft$status$n, " observations"
+    ))
+  
+  expect_output(
+    print(ft_sim),
+    regex = paste0(
+      "^<FrequencyTable>.*", ft_sim$status$n, " observations"
+    ))
+  
+})
+
+test_that("FrequencyTable plots", {
+  
+  expect_s3_class(
+    plot(ft), c("gg", "ggplot")
+  )
+  expect_s3_class(
+    plot(ft_sim), c("gg", "ggplot")
+  )
+})
+
+test_that("FrequencyTable generates summary", {
+  
+  expect_equal(
+    names(summary(ft)),
+    c("n", "min", "max", "mean", "median", "sd", "skewness", "kurtosis")
+  )
+  
+  expect_equal(
+    names(summary(ft_sim)),
+    c("n", "min", "max", "mean", "median", "sd", "skewness", "kurtosis")
+  )
+})
